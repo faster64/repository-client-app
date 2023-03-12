33321c0501d1fb9c4402f05885e5d6f3d6720953
Cuong <faster64>
2023-03-13 00:22:28 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   team: "aws",
   app_allows_guests: false,
-  api_url: "https://amabon-gateway.azurewebsites.net/api/gateway",
-  hub_url: "http://localhost:6001/notify",
+  api_url: "https://localhost:5000/api/v1",
+  hub_url: "https://localhost:6001/notify",
   enableShowError: false,
 };
